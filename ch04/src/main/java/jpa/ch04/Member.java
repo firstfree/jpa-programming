3a27768098f16cc2606b672f745e7d8549f674e2
JesusOh <ojsboy@hotmail.com>
2019-01-05 18:36:18 +0900

    Add constraints

--- ch04/src/main/java/jpa/ch04/Member.java
+++ ch04/src/main/java/jpa/ch04/Member.java
@@ -11,6 +11,7 @@ import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,13 +23,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "MEMBER")
+@Table(name = "MEMBER", uniqueConstraints =
+	@UniqueConstraint(name = "NAME_AGE_UNIQUE", columnNames = {"NAME", "AGE"}))
 public class Member {
 	@Id
 	@Column(name = "ID")
 	private String id;
 	
-	@Column(name = "NAME")
+	@Column(name = "NAME", nullable = false, length = 10)
 	private String username;
 	
 	private Integer age;
