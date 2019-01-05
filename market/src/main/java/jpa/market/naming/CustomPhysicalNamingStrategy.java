89add42c1630c01da92aba3dfd464d974321066d
JesusOh <ojsboy@hotmail.com>
2019-01-05 22:15:39 +0900

    Initial commit for jpa market

---
+++ market/src/main/java/jpa/market/naming/CustomPhysicalNamingStrategy.java
@@ -0,0 +1,45 @@
+package jpa.market.naming;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+public class CustomPhysicalNamingStrategy implements PhysicalNamingStrategy {
+
+	@Override
+	public Identifier toPhysicalCatalogName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+		return convertToSnakeCase(identifier);
+	}
+
+	@Override
+	public Identifier toPhysicalSchemaName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+		return convertToSnakeCase(identifier);
+	}
+
+	@Override
+	public Identifier toPhysicalTableName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+		return convertToSnakeCase(identifier);
+	}
+
+	@Override
+	public Identifier toPhysicalSequenceName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+		return convertToSnakeCase(identifier);
+	}
+
+	@Override
+	public Identifier toPhysicalColumnName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+		return convertToSnakeCase(identifier);
+	}
+
+	private Identifier convertToSnakeCase(final Identifier identifier) {
+		if (identifier == null) {
+			return identifier;
+		}
+		
+		final String regex = "([a-z])([A-Z])";
+		final String replacement = "$1_$2";
+		final String newName = identifier.getText().replaceAll(regex, replacement).toLowerCase();
+		
+		return Identifier.toIdentifier(newName);
+	}
+}
