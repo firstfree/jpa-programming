23dab24f2a02f27f5f4ea975effe639d2e7c018c
JesusOh <ojsboy@hotmail.com>
2019-01-12 12:12:10 +0900

    Add single table strategy

---
+++ ch07/src/main/java/jpa/ch07/s01/single/entity/Album.java
@@ -0,0 +1,25 @@
+package jpa.ch07.s01.single.entity;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@Entity(name = "SingleAlbum")
+@DiscriminatorValue("A")
+public class Album extends Item {
+	
+	private String artist;
+	
+	@Builder
+	public Album(String name, int price, String artist) {
+		super(name, price);
+		this.artist = artist;
+	}
+}
---
+++ ch07/src/main/java/jpa/ch07/s01/single/entity/Movie.java
@@ -0,0 +1,27 @@
+package jpa.ch07.s01.single.entity;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@Entity(name = "SingleMovie")
+@DiscriminatorValue("M")
+public class Movie extends Item {
+	
+	private String director;
+	private String actor;
+	
+	@Builder
+	public Movie(String name, int price, String director, String actor) {
+		super(name, price);
+		this.director = director;
+		this.actor = actor;
+	}
+}
